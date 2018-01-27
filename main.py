0cf300bf46f6f0f4f4a30e0ea8c65250a5ff79ba
Neumi <jan.neumann111@googlemail.com>
2018-01-27 13:02:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 
 address = ('192.168.2.10', 5000) #server IP and port
 client_socket = socket(AF_INET, SOCK_DGRAM) #Socket
-client_socket.settimeout(1)  # wait max 1 sec on answer... if response is expected
+client_socket.settimeout(1)  # wait max 1 sec for answer... if response is expected
 
 while (1):
 	data = "motor-L-20-5-1"  # settings for motor ... do motor, left, 20 steps, 5 ms delay, dir 1
